ee70a7f5b04131e17a29c89f92926e874d38acef
Stephan Allen <sallen202009@gmail.com>
2019-01-28 13:35:18 -0500

    changes

--- lamba/dataRetrieve/index.js
+++ lamba/dataRetrieve/index.js
@@ -1,24 +1,24 @@
 const AWS = require('aws-sdk')
 const rek = new AWS.Rekognition()
 
-const returnFaces = ()=>{
-    return new Promise((resolve,reject)=>{
-        var params = {
-            CollectionId: "rekImages", 
-           }
-        rek.listFaces(params, (err,data)=>{
-            if(err) reject(err)
-            else resolve(data)
-        })
+const returnFaces = () => {
+  return new Promise((resolve, reject) => {
+    var params = {
+      CollectionId: "cam1",
+    }
+    rek.listFaces(params, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
     })
+  })
 
 }
 
-exports.handler = async (event, context) =>{
-    const data = await returnFaces()
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify(data),
-    }
-    return response
+exports.handler = async (event, context) => {
+  const data = await returnFaces()
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(data),
+  }
+  return response
 }
