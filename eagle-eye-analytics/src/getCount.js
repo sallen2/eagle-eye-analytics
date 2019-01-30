3993d725a300f2df2ffe75d823647cecb63533e6
uday <usaches@gmail.com>
2019-01-30 01:08:25 -0500

    case2

--- eagle-eye-analytics/src/getCount.js
+++ eagle-eye-analytics/src/getCount.js
@@ -1,11 +1,8 @@
-
 var request = require("request");
-
 var plotly = require('plotly')("usaches", "HuUTBwnwPucXrvJ0obxI");
 var arr=[];
 var rem=[];
 var dirt = [];
-
 var teetime=[];
 request("https://wzy74zfyd3.execute-api.us-east-1.amazonaws.com/latest", function(error, response, body) {
 
