497c211302090b808ecf931314f46a9cff3f23af
Jrew <jrewmohamed@gmail.com>
2019-02-06 19:23:58 -0500

    Added background test

--- eagle-eye-analytics/src/Components/Description.js
+++ eagle-eye-analytics/src/Components/Description.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import {Grid, Typography, Card, CardActionArea, CardMedia} from '@material-ui/core/';
-import peopleTrackingImage from '../Assets/amazonRekognition.jpg'
+import { Grid, Typography, Card, CardActionArea, CardMedia } from '@material-ui/core/';
+// import peopleTrackingImage from '../Assets/amazonRekognition.jpg'
+import pexelFriends from '../Assets/pexelFriends.png'
 import prismBackground from '../Assets/prism.png'
 
 
@@ -14,25 +15,36 @@ const styles = theme => ({
     root: {
         flexGrow: 1,
         paddingTop: theme.spacing.unit * 20,
-        paddingBottom: theme.spacing.unit * 20,
+        // paddingBottom: theme.spacing.unit * 20,
         paddingLeft: theme.spacing.unit * 5,
         paddingRight: theme.spacing.unit * 5,
         backgroundColor: "lightgrey"
-      },
+    },
     paper: {
-        // color: theme.palette.text.secondary,
-        // backgroundImage: {prismBackground},
         height: "50vh",
+    },
+    card: {
+        // 695 × 594
+        height: 695,
+    },
+    media: {
+        height: "695px",
+        position: "relative",
+        zIndex: 2
     },
     text: {
         lineHeight: 2,
-        paddingTop: "5vh"      
+        paddingTop: "25vh",
+        position: "relative",
+        zIndex: 2,
+        color: "white"
     },
-    card: {
-        // maxWidth: 345,
-    },
-    media: {
-        height: "50vh"
+    backgroundImage: {
+        position: "absolute",
+        zIndex: 1,
+        height: "100%",
+        width: "100%",
+        marginLeft: 0,
     }
 });
 
@@ -40,35 +52,37 @@ function Description(props) {
     const { classes } = props;
 
     return (
-        <div className={classes.root} id="Description">
-            <Grid container spacing={40}>
-                <Grid item xs={12} md={6}>
-                    <Card className={classes.card}>
-                        <CardActionArea>
-                            <CardMedia
-                            className={classes.media}
-                            image={peopleTrackingImage}
-                            title=""
-                            />
-                        </CardActionArea>
-                    </Card>
-                </Grid>
-                <Grid item xs={12} md={6}>
-                    <div className={classes.paper}>
-                        <Typography className={classes.text} variant="h5">
-                            Eagle Eye Analytics is an area analysis software designed
-                            to help users determine crowd size over time at designated
-                            areas. Our team built a custom  API for Netgear Arlo cameras
-                            which allows recorded videos and their thumbnails to be
-                            analyzed by AWS Rekognition. Using data returned by
-                            Rekogntion, Eagle Eye's Proprietary algorithms return a detailed
-                            analysis of how many people were in each area at each time, in real time.
+        <div>
+            <img className={classes.backgroundImage} src={prismBackground} />
+            <div className={classes.root} id="Description">
+                <Grid container spacing={40}>
+                    <Grid item xs={12} md={6}>
+                        <Card className={classes.card} raised="true">
+                            <CardActionArea>
+                                <CardMedia
+                                    className={classes.media}
+                                    image={pexelFriends}
+                                    title=""
+                                />
+                            </CardActionArea>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        <div className={classes.paper}>
+                            <Typography className={classes.text} variant="h5">
+                                Eagle Eye Analytics is an area analysis software designed
+                                to help users determine crowd size over time at designated
+                                areas. Our team built a custom  API for Netgear Arlo cameras
+                                which allows recorded videos and their thumbnails to be
+                                analyzed by AWS Rekognition. Using data returned by
+                                Rekogntion, Eagle Eye's Proprietary algorithms return a detailed
+                                analysis of how many people were in each area at each time, in real time.
                         </Typography>
-                    </div>
+                        </div>
+                    </Grid>
                 </Grid>
-            </Grid>
+            </div>
         </div>
-        
     );
 }
 
--- eagle-eye-analytics/src/Components/HowItWorks.js
+++ eagle-eye-analytics/src/Components/HowItWorks.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import {Grid, Typography, Card, CardActionArea, CardMedia} from '@material-ui/core/';
-import phoneImage from '../Assets/iphoneRekognition.jpg'
+import { Grid, Typography, Card, CardActionArea, CardMedia } from '@material-ui/core/';
+// import pexelIphone from '../Assets/iphoneRekognition.jpg'
+import pexelIphone from '../Assets/pexelIphone.png'
 import prismBackground from '../Assets/prism.png'
 
 
 
-// const backgroundImage =
-//     'https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/JrbItQz/technology-network-loop-background_vxe4qnnce__F0000.png';
+// const backgroundImage = prismBackground;
 
 const styles = theme => ({
     root: {
         flexGrow: 1,
         paddingTop: theme.spacing.unit * 20,
-        paddingBottom: theme.spacing.unit * 20,
+        // paddingBottom: theme.spacing.unit * 20,
         paddingLeft: theme.spacing.unit * 5,
         paddingRight: theme.spacing.unit * 5,
-        backgroundColor: "lightgrey"
-      },
+        backgroundColor: "lightgrey",
+    },
     paper: {
         // color: theme.palette.text.secondary,
         // backgroundImage: {prismBackground},
@@ -26,13 +26,26 @@ const styles = theme => ({
     },
     text: {
         lineHeight: 2,
-        paddingTop: "5vh"      
+        paddingTop: "25vh",
+        position: "relative",
+        zIndex: 2,
+        color: "white"
     },
     card: {
-        // maxWidth: 345,
+        // 911 × 607
+        height: 695,
     },
     media: {
-        height: "50vh"
+        height: "695px",
+        position: "relative",
+        zIndex: 2
+    },
+    backgroundImage: {
+        position: "absolute",
+        zIndex: 1,
+        height: "100%",
+        width: "100%",
+        marginLeft: 0,
     }
 });
 
@@ -40,33 +53,35 @@ function HowItWorks(props) {
     const { classes } = props;
 
     return (
-        <div className={classes.root} id="How">
-            <Grid container spacing={40}>
-            <Grid item xs={12} md={6}>
-                    <div className={classes.paper}>
-                        <Typography className={classes.text} variant="h5">
-                            Eagle Eye's Proprietary algorithms return a detailed analysis of how many people
-                            were in each area at each time, as well as an aggregate number of unique people who
-                            visit each area, tabulated in real time. Eagle Eye Analytics can also take a live
-                            photo of a person and produce an album of thumbnails that were captured containing
-                            that particular person.
+        <div>
+            <img className={classes.backgroundImage} src={prismBackground} />
+            <div className={classes.root} id="How">
+                <Grid container spacing={40}>
+                    <Grid item xs={12} md={6}>
+                        <div className={classes.paper}>
+                            <Typography className={classes.text} variant="h5">
+                                Eagle Eye's Proprietary algorithms return a detailed analysis of how many people
+                                were in each area at each time, as well as an aggregate number of unique people who
+                                visit each area, tabulated in real time. Eagle Eye Analytics can also take a live
+                                photo of a person and produce an album of thumbnails that were captured containing
+                                that particular person.
                         </Typography>
-                    </div>
+                        </div>
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        <Card className={classes.card} raised="true">
+                            <CardActionArea>
+                                <CardMedia
+                                    className={classes.media}
+                                    image={pexelIphone}
+                                    title=""
+                                />
+                            </CardActionArea>
+                        </Card>
+                    </Grid>
                 </Grid>
-                <Grid item xs={12} md={6}>
-                    <Card className={classes.card}>
-                        <CardActionArea>
-                            <CardMedia
-                            className={classes.media}
-                            image={phoneImage}
-                            title=""
-                            />
-                        </CardActionArea>
-                    </Card>
-                </Grid>
-            </Grid>
+            </div>
         </div>
-        
     );
 }
 
