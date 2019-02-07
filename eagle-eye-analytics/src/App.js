8fb2aa0457f0300867611e8824d027e52b240f69
Stephan Allen <sallen202009@gmail.com>
2019-02-07 12:04:05 -0500

    fixed button

--- eagle-eye-analytics/src/App.js
+++ eagle-eye-analytics/src/App.js
@@ -9,16 +9,38 @@ import Button from '@material-ui/core/Button';
 import HowItWorks from './Components/HowItWorks';
 import TheTeam from './Components/TheTeam';
 import backupPlotly from './Assets/newplot.png';
-
+import AWS from 'aws-sdk'
+const lambda = new AWS.Lambda()
 class App extends Component {
 
   state = {
     urlsData: [],
     showCam: false,
+    refresh: 0
+  }
+
+  resetIframe = () => {
+    this.setState({ refresh: this.state.refresh + 1 });
+    console.log('invoked')
   }
 
   showCam = () => {
     this.setState({ showCam: !this.state.showCam })
+  }
+
+  refreshGraph = () => {
+    var params = {
+      FunctionName: 'graphrefresh'
+    }
+    lambda.invoke(params, (err, data) => {
+      if (err) {
+        console.log(err)
+        throw err
+      } else {
+        this.resetIframe()
+        console.log(data)
+      }
+    })
   }
 
   render() {
@@ -29,15 +51,16 @@ class App extends Component {
         <Description />
         <HowItWorks />
         <TheTeam />
-        <Demo />
+        <Demo  refresh={this.state.refresh} />
         {/* <img src={backupPlotly} /> */}
         <div style={{ paddingLeft: '5%' }}>
-          {this.state.showCam ? <WebCamScan /> : null}
+          {this.state.showCam ? <WebCamScan refresh={this.resetIframe} /> : null}
         </div>
-        <Button 
-          style={{ marginLeft: '47%', marginBottom: '5%' }} 
-          variant="outlined" 
-          onClick={this.showCam} 
+        <Button style={{ marginLeft: '46.2%', marginBottom: '5%' }} variant="outlined" color="primary" onClick={this.refreshGraph}>Refresh Graph</Button>
+        <Button
+          style={{ marginLeft: '47%', marginBottom: '5%' }}
+          variant="outlined"
+          onClick={this.showCam}
           color="primary">
           Toggle Cam
         </Button>
